507685e464ade120bb7ae330ff4375a4ab1b957c
Delphine <del3bre@gmail.com>
2019-01-11 16:09:06 +0100

    DAO update to spring

--- src/main/java/com/cgi/dao/CategoryDaoImpl.java
+++ src/main/java/com/cgi/dao/CategoryDaoImpl.java
@@ -3,149 +3,47 @@ package com.cgi.dao;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
 
 import com.cgi.model.Category;
-import com.cgi.utils.Application;
 
+@Repository
+@Transactional
 public class CategoryDaoImpl implements CategoryDao{
 
+	@PersistenceContext
+	EntityManager em;
+	
 	@Override
 	public List<Category> findAll() {
-		EntityManager em = null;
-
-		EntityManagerFactory emf = Application.getInstance().getEmf();
-		em = emf.createEntityManager();
-
-		List<Category> l = em.createQuery("select c from Classe c").getResultList();
-
-		return l;
-
+		return em.createQuery("from Category c").getResultList();
 	}
 
 	@Override
 	public Category findByKey(Integer key) {
-		EntityManager em = null;
-
-		EntityManagerFactory emf = Application.getInstance().getEmf();
-		em = emf.createEntityManager();
-
-		Category c = em.find(Category.class, key);
-
-		return c;
+		return em.find(Category.class, key);
 	}
 
 	@Override
 	public void insert(Category obj) {
-		EntityManager em = null;
-
-		try {
-			EntityManagerFactory emf = Application.getInstance().getEmf();
-			em = emf.createEntityManager();
-
-			em.getTransaction().begin();
-
-			em.persist(obj);
-			em.getTransaction().commit();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			if (em.getTransaction() != null) {
-				em.getTransaction().rollback();
-			}
-		} finally {
-			if (em != null) {
-				em.close();
-			}
-		}
+		em.persist(obj);
 	}
 
 	@Override
 	public void update(Category obj) {
-		EntityManager em = null;
-
-		try {
-			EntityManagerFactory emf = Application.getInstance().getEmf();
-			em = emf.createEntityManager();
-
-			em.getTransaction().begin();
-			em.merge(obj);
-			em.getTransaction().commit();
-		} catch (Exception e) {
-			e.printStackTrace();
-			if (em.getTransaction() != null) {
-				em.getTransaction().rollback();
-			}
-		} finally {
-			if (em != null) {
-				em.close();
-			}
-		}
+		em.merge(obj);
 	}
 
 	@Override
 	public void delete(Category obj) {
-		EntityManager em = null;
-		try {
-			EntityManagerFactory emf = Application.getInstance().getEmf();
-			em = emf.createEntityManager();
-
-			Category c = em.find(Category.class, obj.getIdCat());
-
-			if (c != null) {
-				System.out.println("found");
-				em.getTransaction().begin();
-				em.remove(c);
-				em.getTransaction().commit();
-				System.out.println("removed");
-
-			} else {
-				System.out.println("not found");
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			if (em.getTransaction() != null) {
-				em.getTransaction().rollback();
-			}
-		} finally {
-			if (em != null) {
-				em.close();
-			}
-		}
-
+		em.remove(em.merge(obj));
 	}
 
 	@Override
 	public void deleteByKey(Integer key) {
-		EntityManager em = null;
-
-		try {
-			EntityManagerFactory emf = Application.getInstance().getEmf();
-			em = emf.createEntityManager();
-
-			Category c = em.find(Category.class, key);
-
-			if (c != null) {
-				System.out.println("found");
-				em.getTransaction().begin();
-				em.remove(c);
-				em.getTransaction().commit();
-				System.out.println("removed");
-
-			} else {
-				System.out.println("not found");
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			if (em.getTransaction() != null) {
-				em.getTransaction().rollback();
-			}
-		} finally {
-			if (em != null) {
-				em.close();
-			}
-
-		}
+		em.remove(em.find(Category.class, key));
 	}
 }
--- src/main/java/com/cgi/dao/VoteDaoImpl.java
+++ src/main/java/com/cgi/dao/VoteDaoImpl.java
@@ -3,150 +3,42 @@ package com.cgi.dao;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
 
 import com.cgi.model.Vote;
-import com.cgi.model.Vote;
-import com.cgi.utils.Application;
 
 public class VoteDaoImpl implements VoteDao{
+	
+	@PersistenceContext
+	EntityManager em;
 
 	@Override
 	public List<Vote> findAll() {
-		EntityManager em = null;
-
-		EntityManagerFactory emf = Application.getInstance().getEmf();
-		em = emf.createEntityManager();
-
-		List<Vote> l = em.createQuery("select c from Vote c").getResultList();
-
-		return l;
-
+		return em.createQuery("from Vote v").getResultList();
 	}
 
 	@Override
 	public Vote findByKey(Integer key) {
-		EntityManager em = null;
-
-		EntityManagerFactory emf = Application.getInstance().getEmf();
-		em = emf.createEntityManager();
-
-		Vote c = em.find(Vote.class, key);
-
-		return c;
+		return em.find(Vote.class, key);
 	}
 
 	@Override
 	public void insert(Vote obj) {
-		EntityManager em = null;
-
-		try {
-			EntityManagerFactory emf = Application.getInstance().getEmf();
-			em = emf.createEntityManager();
-
-			em.getTransaction().begin();
-
 			em.persist(obj);
-			em.getTransaction().commit();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			if (em.getTransaction() != null) {
-				em.getTransaction().rollback();
-			}
-		} finally {
-			if (em != null) {
-				em.close();
-			}
-		}
 	}
 
 	@Override
 	public void update(Vote obj) {
-		EntityManager em = null;
-
-		try {
-			EntityManagerFactory emf = Application.getInstance().getEmf();
-			em = emf.createEntityManager();
-
-			em.getTransaction().begin();
 			em.merge(obj);
-			em.getTransaction().commit();
-		} catch (Exception e) {
-			e.printStackTrace();
-			if (em.getTransaction() != null) {
-				em.getTransaction().rollback();
-			}
-		} finally {
-			if (em != null) {
-				em.close();
-			}
-		}
 	}
 
 	@Override
 	public void delete(Vote obj) {
-		EntityManager em = null;
-		try {
-			EntityManagerFactory emf = Application.getInstance().getEmf();
-			em = emf.createEntityManager();
-
-			Vote c = em.find(Vote.class, obj.getIdVote());
-
-			if (c != null) {
-				System.out.println("found");
-				em.getTransaction().begin();
-				em.remove(c);
-				em.getTransaction().commit();
-				System.out.println("removed");
-
-			} else {
-				System.out.println("not found");
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			if (em.getTransaction() != null) {
-				em.getTransaction().rollback();
-			}
-		} finally {
-			if (em != null) {
-				em.close();
-			}
-		}
-
+		em.remove(em.find(Vote.class, obj.getIdVote()));
 	}
 
 	@Override
 	public void deleteByKey(Integer key) {
-		EntityManager em = null;
-
-		try {
-			EntityManagerFactory emf = Application.getInstance().getEmf();
-			em = emf.createEntityManager();
-
-			Vote c = em.find(Vote.class, key);
-
-			if (c != null) {
-				System.out.println("found");
-				em.getTransaction().begin();
-				em.remove(c);
-				em.getTransaction().commit();
-				System.out.println("removed");
-
-			} else {
-				System.out.println("not found");
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			if (em.getTransaction() != null) {
-				em.getTransaction().rollback();
-			}
-		} finally {
-			if (em != null) {
-				em.close();
-			}
-
-		}
+		em.remove(em.find(Vote.class, key));
 	}
 }
