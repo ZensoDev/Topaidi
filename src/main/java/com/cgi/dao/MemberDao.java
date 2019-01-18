fb7b4b6a690e31149597af1199cd51d847b5d49d
ZensoDev <zenso2@yahoo.fr>
2019-01-18 12:05:19 +0100

    member Dao update

--- src/main/java/com/cgi/dao/MemberDao.java
+++ src/main/java/com/cgi/dao/MemberDao.java
@@ -5,4 +5,7 @@ import com.cgi.model.Member;
 public interface MemberDao extends DAOGeneric<Member, Integer>{
 
 	Member findByMail(String loginMail);
+	boolean existingMail(String login);
+	Member findByMailPwd(String loginMail, String password);
+	boolean existingMailPwd(String login, String password);
 }
